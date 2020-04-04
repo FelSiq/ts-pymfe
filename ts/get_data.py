a03bd305652f182ba8bf7002a7e978ace3eb288a
felsiq <felipe.siqueira@usp.br>
2020-04-04 04:35:51 +0300

    Added new data

--- ts/get_data.py
+++ ts/get_data.py
@@ -1,7 +1,14 @@
 import numpy as np
 import pandas as pd
 
+filepath = (
+    ("data/0/CalIt2.data", 2),
+    ("data/1/sales-of-shampoo-over-a-three-ye.csv", 1),
+    ("data/2/monthly-sunspots.csv", 1),
+)
 
-def load_data() -> np.ndarray:
-    data = pd.read_csv("data/1/sales-of-shampoo-over-a-three-ye.csv")
-    return data.iloc[:, 1].values
+
+def load_data(data_id: int = 1) -> np.ndarray:
+    fp, ts_ind = filepath[data_id]
+    data = pd.read_csv(fp)
+    return data.iloc[:, ts_ind].values
