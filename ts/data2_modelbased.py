b7193dee6dd4651e68912bd15e1de5b2b35b837e
felsiq <felipe.siqueira@usp.br>
2020-04-08 20:04:39 +0300

    Updated model-based module

--- ts/data2_modelbased.py
+++ ts/data2_modelbased.py
@@ -10,22 +10,43 @@ import data1_detrend
 
 class MFETSModelBased:
     @classmethod
-    def ft_ets_double(cls, ts: np.ndarray, ts_period: int, damped: bool = False) -> np.ndarray:
+    def precompute_model_ets(
+        cls,
+        ts: np.ndarray,
+        ts_period: int,
+        damped: bool = False,
+        **kwargs,
+    ) -> t.Dict[str, statsmodels.tsa.holtwinters.HoltWintersResultsWrapper]:
+        """TODO."""
+        precomp_vals = {}
+
+        if "model_ets_double" not in kwargs:
+            model = cls._fit_model_ets_double(ts=ts, damped=damped)
+            precomp_vals["model_ets_double"] = model
+
+            model = cls._fit_model_ets_triple(ts=ts, damped=damped)
+            precomp_vals["model_ets_triple"] = model
+
+        return precomp_vals
+
+    @staticmethod
+    def _fit_model_ets_double(
+        ts: np.ndarray,
+        damped: bool = False
+    ) -> statsmodels.tsa.holtwinters.HoltWintersResultsWrapper:
+        """TODO."""
         model = statsmodels.tsa.holtwinters.ExponentialSmoothing(
-            endog=ts,
-            trend="additive",
-            damped=damped,
-            seasonal=None).fit()
+            endog=ts, trend="additive", damped=damped, seasonal=None).fit()
 
-        params = model.params_formatted["param"]
+        return model
 
-        alpha = params["smoothing_level"]
-        beta = params["smoothing_slope"]
-
-        return alpha, beta
-
-    @classmethod
-    def ft_ets_triple(cls, ts: np.ndarray, ts_period: int, damped: bool = True) -> np.ndarray:
+    @staticmethod
+    def _fit_model_ets_triple(
+        ts: np.ndarray,
+        ts_period: int,
+        damped: bool = False,
+    ) -> statsmodels.tsa.holtwinters.HoltWintersResultsWrapper:
+        """TODO."""
         model = statsmodels.tsa.holtwinters.ExponentialSmoothing(
             endog=ts,
             trend="additive",
@@ -33,13 +54,92 @@ class MFETSModelBased:
             damped=damped,
             seasonal_periods=ts_period).fit()
 
-        params = model.params_formatted["param"]
+        return model
 
-        alpha = params["smoothing_level"]
-        beta = params["smoothing_slope"]
-        gamma = params["smoothing_seasonal"]
+    @classmethod
+    def ft_ets_double_alpha(
+        cls,
+        ts: np.ndarray,
+        damped: bool = False,
+        model_ets_double: t.Optional[
+            statsmodels.tsa.holtwinters.HoltWintersResultsWrapper] = None
+    ) -> float:
+        if model_ets_double is None:
+            model_ets_double = cls._fit_model_ets_double(ts=ts, damped=damped)
 
-        return alpha, beta, gamma
+        alpha = model_ets_double.params_formatted["param"]["smoothing_level"]
+
+        return alpha
+
+    @classmethod
+    def ft_ets_double_beta(
+        cls,
+        ts: np.ndarray,
+        damped: bool = False,
+        model_ets_double: t.Optional[
+            statsmodels.tsa.holtwinters.HoltWintersResultsWrapper] = None,
+    ) -> float:
+        if model_ets_double is None:
+            model_ets_double = cls._fit_model_ets_double(ts=ts, damped=damped)
+
+        beta = model_ets_double.params_formatted["param"]["smoothing_slope"]
+
+        return beta
+
+    @classmethod
+    def ft_ets_triple_alpha(
+        cls,
+        ts: np.ndarray,
+        ts_period: int,
+        damped: bool = True,
+        model_ets_triple: t.Optional[
+            statsmodels.tsa.holtwinters.HoltWintersResultsWrapper] = None,
+    ) -> float:
+        if model_ets_triple is None:
+            model_ets_triple = cls._fit_model_ets_triple(ts=ts,
+                                                         ts_period=ts_period,
+                                                         damped=damped)
+
+        alpha = model_ets_triple.params_formatted["param"]["smoothing_level"]
+
+        return alpha
+
+    @classmethod
+    def ft_ets_triple_beta(
+        cls,
+        ts: np.ndarray,
+        ts_period: int,
+        damped: bool = True,
+        model_ets_triple: t.Optional[
+            statsmodels.tsa.holtwinters.HoltWintersResultsWrapper] = None,
+    ) -> float:
+        if model_ets_triple is None:
+            model_ets_triple = cls._fit_model_ets_triple(ts=ts,
+                                                         ts_period=ts_period,
+                                                         damped=damped)
+
+        beta = model_ets_triple.params_formatted["param"]["smoothing_slope"]
+
+        return beta
+
+    @classmethod
+    def ft_ets_triple_gamma(
+        cls,
+        ts: np.ndarray,
+        ts_period: int,
+        damped: bool = True,
+        model_ets_triple: t.Optional[
+            statsmodels.tsa.holtwinters.HoltWintersResultsWrapper] = None,
+    ) -> float:
+        if model_ets_triple is None:
+            model_ets_triple = cls._fit_model_ets_triple(ts=ts,
+                                                         ts_period=ts_period,
+                                                         damped=damped)
+
+        gamma = model_ets_triple.params_formatted["param"][
+            "smoothing_seasonal"]
+
+        return gamma
 
 
 def _test() -> None:
@@ -49,11 +149,20 @@ def _test() -> None:
     ts_trend, ts_season, ts_residuals = data1_detrend.decompose(
         ts, ts_period=ts_period)
     ts = ts.to_numpy()
-    
-    res = MFETSModelBased.ft_ets_double(ts, ts_period=ts_period)
+
+    res = MFETSModelBased.ft_ets_double_alpha(ts)
     print(res)
 
-    res = MFETSModelBased.ft_ets_triple(ts, ts_period=ts_period)
+    res = MFETSModelBased.ft_ets_double_beta(ts)
+    print(res)
+
+    res = MFETSModelBased.ft_ets_triple_alpha(ts, ts_period)
+    print(res)
+
+    res = MFETSModelBased.ft_ets_triple_beta(ts, ts_period)
+    print(res)
+
+    res = MFETSModelBased.ft_ets_triple_gamma(ts, ts_period)
     print(res)
 
 
