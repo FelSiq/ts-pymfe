90aba0b695faa0c66c0b6a1b534c3477bf04b76d
felsiq <felipe.siqueira@usp.br>
2020-05-09 02:03:12 +0300

    Added function to find critical points

--- ts/_utils.py
+++ ts/_utils.py
@@ -1,4 +1,5 @@
 import typing as t
+import operator
 
 import sklearn.preprocessing
 import numpy as np
@@ -101,10 +102,108 @@ def sample_data(ts: np.ndarray,
     return ts[:threshold]
 
 
-def standardize_ts(ts: np.ndarray, ts_scaled: t.Optional[np.ndarray] = None) -> np.ndarray:
+def standardize_ts(ts: np.ndarray,
+                   ts_scaled: t.Optional[np.ndarray] = None) -> np.ndarray:
     """Standardize (z-score normalization) time-series."""
     if ts_scaled is None:
         return sklearn.preprocessing.StandardScaler().fit_transform(
             ts.reshape(-1, 1)).ravel()
 
     return ts
+
+
+def find_plateau_pt(arr: np.ndarray,
+                    arr_diff: t.Optional[np.ndarray] = None) -> np.ndarray:
+    """Find plateau points in array."""
+    if arr_diff is None:
+        arr_diff = np.diff(arr)
+
+    arr_diff_2 = np.diff(arr_diff)
+
+    res = np.logical_and(np.isclose(arr_diff_2, 0),
+                         np.isclose(arr_diff[:-1], 0))
+
+    return np.hstack((0, res.astype(int), 0))
+
+
+def find_crit_pt(arr: np.ndarray, type_: str) -> np.ndarray:
+    """Find critical points on the given values.
+
+    ``type`` must be in {"min", "max", "plateau", "non-plateau", "any"}.
+    """
+    if arr.size <= 2:
+        raise ValueError("Array too small (size {}). Need at least "
+                         "3 elements.".format(arr.size))
+
+    VALID_TYPES = {"min", "max", "plateau", "non-plateau", "any"}
+
+    if type_ not in VALID_TYPES:
+        raise ValueError("'type_' must be in {} (got '{}')."
+                         "".format(type_, VALID_TYPES))
+
+    # Note: first discrete derivative
+    arr_diff_1 = np.diff(arr)
+
+    if type_ == "plateau":
+        return find_plateau_pt(arr, arr_diff_1)
+
+    turning_pt = arr_diff_1[1:] * arr_diff_1[:-1] < 0
+
+    if type_ == "non-plateau":
+        return np.hstack((0, turning_pt.astype(int), 0))
+
+    if type_ == "any":
+        plat = find_plateau_pt(arr, arr_diff_1)
+        turning_pt = np.hstack((0, turning_pt.astype(int), 0))
+        res = np.logical_or(turning_pt, plat)
+        return res
+
+    # Note: second discrete derivative
+    arr_diff_2 = np.diff(arr_diff_1)
+
+    rel = operator.lt if type_ == "max" else operator.gt
+
+    interest_pt = rel(arr_diff_2, 0)
+    local_m = np.logical_and(turning_pt, interest_pt)
+
+    return np.hstack((0, local_m.astype(int), 0))
+
+
+def _test() -> None:
+    import matplotlib.pyplot as plt
+    np.random.seed(16)
+    arr = 10 * np.random.random(15)
+    arr[5] = arr[6] = arr[7]
+
+    pts_1 = find_crit_pt(arr, "any")
+    pts_2 = find_crit_pt(arr, "non-plateau")
+    pts_3 = find_crit_pt(arr, "plateau")
+    pts_4 = find_crit_pt(arr, "min")
+    pts_5 = find_crit_pt(arr, "max")
+
+    print(pts_1.shape)
+    print(pts_2.shape)
+    print(pts_3.shape)
+    print(pts_4.shape)
+    print(pts_5.shape)
+
+    time = np.arange(arr.size)
+    plt.vlines(x=time, ymin=0, ymax=21, linestyle="dotted", color="gray")
+    plt.hlines(y=np.arange(10),
+               xmin=0,
+               xmax=arr.size - 1,
+               linestyle="dotted",
+               color="black")
+    plt.plot(time, 10 + arr, label="time series")
+    plt.scatter(time, 8 + pts_1, label="any")
+    plt.scatter(time, 6 + pts_2, label="non-plateau")
+    plt.scatter(time, 4 + pts_3, label="plateau")
+    plt.scatter(time, 2 + pts_4, label="min")
+    plt.scatter(time, 0 + pts_5, label="max")
+    plt.ylim((-10, 21.0))
+    plt.legend(loc="lower center")
+    plt.show()
+
+
+if __name__ == "__main__":
+    _test()
