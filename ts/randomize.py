6f4e3838b0abd01ac1e06811f30e64be905bfb1b
felsiq <felipe.siqueira@usp.br>
2020-05-17 19:52:40 +0300

    Added randomize module

---
+++ ts/randomize.py
@@ -0,0 +1,141 @@
+import typing as t
+
+import numpy as np
+import scipy.signal
+import scipy.stats
+
+import _utils
+import _detrend
+import _period
+import _get_data
+
+
+class MFETSRandomize:
+    @classmethod
+    def _randomize_stat(cls,
+                        ts: np.ndarray,
+                        func_stats: t.Sequence[t.Callable[[np.ndarray],
+                                                          float]],
+                        strategy: str = "dist-static",
+                        prop_rep: t.Union[int, float] = 2,
+                        prop_interval: float = 0.1,
+                        ts_scaled: t.Optional[np.ndarray] = None,
+                        random_state: t.Optional[int] = None) -> np.ndarray:
+        """TODO."""
+        if prop_rep <= 0:
+            raise ValueError(
+                "'prop_rep' must be positive (got {}).".format(prop_rep))
+
+        if prop_interval <= 0:
+            raise ValueError(
+                "'prop_interval' must be positive (got {}).".format(
+                    prop_interval))
+
+        VALID_STRATEGY = ("dist-static", "dist-dynamic", "permute")
+
+        if strategy not in VALID_STRATEGY:
+            raise ValueError("'strategy' not in {} (got '{}')."
+                             "".format(VALID_STRATEGY, strategy))
+
+        if not hasattr(func_stats, "__len__"):
+            func_stats = [func_stats]
+
+        ts_scaled = _utils.standardize_ts(ts=ts, ts_scaled=ts_scaled)
+
+        rep_it = int(np.ceil(prop_interval * ts_scaled.size))
+
+        # Note: adding (num_it % rep_it) to avoid lose computation of
+        # the remaining iterations that do not produce a statistic.
+        num_it = int(np.ceil(prop_rep * ts_scaled.size))
+        num_it += (num_it % rep_it)
+
+        res = np.zeros((len(func_stats), 1 + num_it // rep_it))
+        ts_rnd = np.copy(ts_scaled)
+        ts_src = ts_scaled if strategy == "dist-static" else ts_rnd
+        swap = strategy == "permute"
+        stat_ind = 0
+
+        if random_state is not None:
+            np.random.seed(random_state)
+
+        inds_rnd = np.random.randint(ts_scaled.size, size=(num_it, 2))
+
+        for it, (ind_a, ind_b) in enumerate(inds_rnd):
+            if swap:
+                ts_rnd[ind_a], ts_src[ind_b] = ts_src[ind_b], ts_rnd[ind_a]
+
+            else:
+                ts_rnd[ind_a] = ts_src[ind_b]
+
+            if it % rep_it == 0:
+                for ind_f, func in enumerate(func_stats):
+                    res[ind_f, stat_ind] = func(ts_rnd)
+
+                stat_ind += 1
+
+        return res if len(func_stats) > 1 else res.ravel()
+
+    @classmethod
+    def ft_randomize_mean(
+            cls,
+            ts: np.ndarray,
+            strategy: str = "dist-dynamic",
+            prop_rep: t.Union[int, float] = 2,
+            prop_interval: float = 0.1,
+            ts_scaled: t.Optional[np.ndarray] = None,
+            random_state: t.Optional[int] = None,
+            rand_stat_mean: t.Optional[np.ndarray] = None) -> np.ndarray:
+        """TODO."""
+        if rand_stat_mean is not None:
+            return rand_stat_mean
+
+        res = cls._randomize_stat(ts=ts,
+                                  func_stats=np.mean,
+                                  strategy=strategy,
+                                  prop_rep=prop_rep,
+                                  prop_interval=prop_interval,
+                                  random_state=random_state,
+                                  ts_scaled=ts_scaled)
+
+        return res
+
+    @classmethod
+    def ft_randomize_sd(
+            cls,
+            ts: np.ndarray,
+            strategy: str = "dist-dynamic",
+            prop_rep: t.Union[int, float] = 2,
+            prop_interval: float = 0.1,
+            ts_scaled: t.Optional[np.ndarray] = None,
+            random_state: t.Optional[int] = None,
+            rand_stat_mean: t.Optional[np.ndarray] = None) -> np.ndarray:
+        """TODO."""
+        if rand_stat_mean is not None:
+            return rand_stat_mean
+
+        res = cls._randomize_stat(ts=ts,
+                                  func_stats=np.std,
+                                  strategy=strategy,
+                                  prop_rep=prop_rep,
+                                  prop_interval=prop_interval,
+                                  random_state=random_state,
+                                  ts_scaled=ts_scaled)
+
+        return res
+
+
+def _test() -> None:
+    ts = _get_data.load_data(3)
+    ts_period = _period.ts_period(ts=ts)
+    ts_trend, ts_season, ts_residuals = _detrend.decompose(ts,
+                                                           ts_period=ts_period)
+
+    res = MFETSRandomize.ft_randomize_mean(ts)
+    print(res)
+
+    res = MFETSRandomize.ft_randomize_sd(ts)
+    print(res)
+
+
+if __name__ == "__main__":
+    _test()
